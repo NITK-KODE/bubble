99096eee041828495fbf8de5324fe0532a7afdfe
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-06-14 08:18:25 +0530

    tweaked UI a little

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -9,6 +9,25 @@ class AccountController < ApplicationController
         end
     end
 
+    def setting
+
+    end
+
+    def pinchange
+        user = Current.member
+        if (user.authenticate(params[:oldpin]) && params[:newpin] == params[:newpinconfirmation])
+            flash[:notice] = "Sucessfully changed pin"
+            user.password = params[:newpin]
+            user.password_confirmation = params[:newpinconfirmation]
+            user.save
+            redirect_to account_path
+
+        else
+            flash.now[:notice] = "Please try again"
+
+            render :setting
+        end
+    end
 
 
     private 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "main#index"
   get "/account/login/" , to: "sessions#new" , as:"sign_in_page"
   post "/account/login/" , to: "sessions#create" , as:"sign_in"
+  get "/settings" , to: "account#setting" , as: "pinchange"
+  post "/settings" , to: "account#pinchange" , as: "pinupdate"
+
   post "/message/" , to:"message#create" , as:"message"
   get "/account/" , to:"account#index" , as:"account"
   get "/project/:id" , to:"projects#index" , as:"project"
