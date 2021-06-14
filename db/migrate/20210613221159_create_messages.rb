ce4f00d57751e35109d4268a2d6f3e518225c8b4
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-06-14 06:47:03 +0530

    configured

--- db/migrate/20210613221159_create_messages.rb
+++ db/migrate/20210613221159_create_messages.rb
@@ -1,6 +1,5 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
-    drop_table :messages
     create_table :messages do |t|
       t.references :member, null: false, foreign_key: true
       t.string :body , null: false
