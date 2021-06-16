66a2c5da881b667d1fd6753b3cb87ca12078fe87
cjphonehomec@gmail.com <cjphonehomec@gmail.com>
2021-06-16 19:42:05 +0530

    styled project_card

--- app/javascript/packs/test.js
+++ app/javascript/packs/test.js
@@ -1,6 +1,15 @@
 
 	document.addEventListener( 'DOMContentLoaded', function () {
-		new Splide( '.splide' , {
+		let project_type_ele = document.getElementById("project_type");
+		let splide = new Splide( '.splide' , {
             gap:"1em"
         }).mount();
+
+		let project_types = ["Completed" , "Ongoing" , "Future"];
+		splide.on('active' , (Slide)=>{
+			console.log(Slide.index);
+			project_type_ele.textContent = project_types[Slide.index]
+		});
+
+
 	} );
